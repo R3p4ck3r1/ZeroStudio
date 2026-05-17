52292421673a0e78f1ce6e8f8e475a212d6aab73
android_zero <31156953+msmt2018@users.noreply.github.com>
2026-05-18 00:16:50 +0800

    Use AI Glasses preview drawable for aiGlassesActivity template thumb

--- utilities/templates-impl/src/main/java/com/itsaky/androidide/templates/impl/androidstudio/activity/aiGlassesActivity/aiGlassesActivityTemplate.kt
+++ utilities/templates-impl/src/main/java/com/itsaky/androidide/templates/impl/androidstudio/activity/aiGlassesActivity/aiGlassesActivityTemplate.kt
@@ -22,7 +22,7 @@ fun aiGlassesActivityProject(): ProjectTemplate =
         projectVersionData = ProjectVersionData(kotlin = composeKotlinVersion),
     ) {
       templateName = R.string.template_ai_glasses_activity
-      thumb = R.drawable.template_empty_activity
+      thumb = R.drawable.template_ai_glasses_activity
       description = string.title_template_description_ai_glasses_activity
 
       defaultAppModule(addAndroidX = false) {
