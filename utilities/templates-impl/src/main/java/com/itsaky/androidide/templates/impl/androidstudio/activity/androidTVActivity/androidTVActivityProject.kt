3b79502f8f59a77fa5fa3eec8117a56b9f74498e
android_zero <31156953+msmt2018@users.noreply.github.com>
2026-05-20 04:42:08 +0800

    feat(templates): scaffold androidTVActivity templates-api implementation assets

---
+++ utilities/templates-impl/src/main/java/com/itsaky/androidide/templates/impl/androidstudio/activity/androidTVActivity/androidTVActivityProject.kt
@@ -0,0 +1,14 @@
+package com.itsaky.androidide.templates.impl.androidstudio.activity.androidTVActivity
+
+import com.itsaky.androidide.templates.ProjectTemplate
+import com.itsaky.androidide.templates.base.modules.android.defaultAppModule
+import com.itsaky.androidide.templates.impl.base.createRecipe
+import com.itsaky.androidide.templates.impl.baseProjectImpl
+
+fun androidTVActivityProject(): ProjectTemplate = baseProjectImpl {
+  defaultAppModule {
+    recipe = createRecipe {
+      // TODO: wire androidTVActivityRecipe to templates-api module data surface.
+    }
+  }
+}
