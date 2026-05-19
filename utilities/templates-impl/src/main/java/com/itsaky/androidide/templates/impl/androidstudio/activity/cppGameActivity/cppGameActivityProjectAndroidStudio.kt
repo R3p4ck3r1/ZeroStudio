82311a395bed0572b56fc69dea30883ac6adbabb
android_zero <31156953+msmt2018@users.noreply.github.com>
2026-05-20 05:30:35 +0800

    feat(templates): migrate cppEmptyActivity and cppGameActivity payloads

---
+++ utilities/templates-impl/src/main/java/com/itsaky/androidide/templates/impl/androidstudio/activity/cppGameActivity/cppGameActivityProjectAndroidStudio.kt
@@ -0,0 +1,14 @@
+package com.itsaky.androidide.templates.impl.androidstudio.activity.cppGameActivity
+
+import com.itsaky.androidide.templates.ProjectTemplate
+import com.itsaky.androidide.templates.base.modules.android.defaultAppModule
+import com.itsaky.androidide.templates.impl.base.createRecipe
+import com.itsaky.androidide.templates.impl.baseProjectImpl
+
+fun cppGameActivityProjectAndroidStudio(): ProjectTemplate = baseProjectImpl {
+  defaultAppModule {
+    recipe = createRecipe {
+      // TODO: wire cppGameActivity recipe with templates-api module data.
+    }
+  }
+}
