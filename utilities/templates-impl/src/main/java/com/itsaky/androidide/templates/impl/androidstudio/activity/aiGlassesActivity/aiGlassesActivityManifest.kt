26500bf7642ceeeb249f81e0625877438bea9cd1
android-zeros <16684848382@163.com>
2026-05-20 02:27:32 +0800

    再次移除无用文件

--- utilities/templates-impl/src/main/java/com/itsaky/androidide/templates/impl/androidstudio/activity/aiGlassesActivity/aiGlassesActivityManifest.kt
+++
@@ -1,5 +0,0 @@
-package com.itsaky.androidide.templates.impl.androidstudio.activity.aiGlassesActivity
-
-// Kept for Android Studio template parity. Manifest generation is implemented via templates-api
-// manifest DSL in aiGlassesActivityRecipe.kt.
-internal const val aiGlassesActivityManifestMarker = "aiGlassesActivityManifest"
