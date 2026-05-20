518c935c04c322022bb97a74ebef0cbe99f31512
android_zero <31156953+msmt2018@users.noreply.github.com>
2026-05-20 22:59:45 +0800

    app: send tooling client capabilities during project init

--- core/app/src/main/java/com/itsaky/androidide/activities/editor/ProjectHandlerActivity.kt
+++ core/app/src/main/java/com/itsaky/androidide/activities/editor/ProjectHandlerActivity.kt
@@ -50,6 +50,7 @@ import com.itsaky.androidide.tasks.executeAsyncProvideError
 import com.itsaky.androidide.tasks.executeWithProgress
 import com.itsaky.androidide.tooling.api.messages.AndroidInitializationParams
 import com.itsaky.androidide.tooling.api.messages.InitializeProjectParams
+import com.itsaky.androidide.tooling.api.messages.ToolingClientCapabilities
 import com.itsaky.androidide.tooling.api.messages.result.InitializeResult
 import com.itsaky.androidide.tooling.api.messages.result.TaskExecutionResult
 import com.itsaky.androidide.tooling.api.messages.result.TaskExecutionResult.Failure.PROJECT_DIRECTORY_INACCESSIBLE
@@ -73,6 +74,7 @@ import java.util.stream.Collectors
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.withContext
+import org.gradle.tooling.events.OperationType
 
 /** @author Akash Yadav */
 @Suppress("MemberVisibilityCanBePrivate")
@@ -443,10 +445,26 @@ abstract class ProjectHandlerActivity : BaseEditorActivity() {
       projectDir: File,
       buildVariants: Map<String, String>,
   ): InitializeProjectParams {
+    val capabilities =
+        ToolingClientCapabilities(
+            requestedOperationTypes =
+                linkedSetOf(
+                    OperationType.TASK,
+                    OperationType.PROJECT_CONFIGURATION,
+                    OperationType.FILE_DOWNLOAD,
+                    OperationType.TRANSFORM,
+                    OperationType.WORK_ITEM,
+                    OperationType.GENERIC,
+                ),
+            maxEventsPerSecond = 120,
+            preferLightweightSync = false,
+        )
+
     return InitializeProjectParams(
         projectDir.absolutePath,
         gradleDistributionParams,
         createAndroidParams(buildVariants),
+        capabilities,
     )
   }
 
@@ -522,6 +540,11 @@ abstract class ProjectHandlerActivity : BaseEditorActivity() {
   }
 
   protected open fun onProjectInitialized(result: InitializeResult) {
+    log.info(
+        "Tooling init negotiated operation types: {}",
+        result.negotiatedOperationTypes,
+    )
+
     val manager = ProjectManagerImpl.getInstance()
     if (isFromSavedInstance && manager.projectInitialized && result == manager.cachedInitResult) {
       log.debug("Not setting up project as this a configuration change")
